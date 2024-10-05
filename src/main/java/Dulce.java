bc27eeeacda9b29c3a8d5a17ad399162ed21ee7a
CrissIHT <115413143+CrissIHT@users.noreply.github.com>
2024-10-05 01:02:22 +0100

    ClaseFinalDulce.java
    Finalizacion de la clase Dulce

--- src/main/java/Dulce.java
+++ src/main/java/Dulce.java
@@ -1,33 +1,66 @@
+/**
+ * Clase abstracta que representa un dulce, que es un tipo de producto.
+ */
 abstract class Dulce extends Producto {
-    private int serie;
-
-    public Dulce(int serie){
-        this.serie = serie;
-    }
-
-    public int getSerie(){
-        return serie;
-    }
-
-    public abstract String comer();
-}
-
-class Snickers extends Dulce{
-    public Snickers(int serie) {
-        super(serie);
-    }
-
-    public String comer() {
-        return "snickers";
+    public Dulce() {
+        // Constructor vacío
     }
 }
 
-class Super8 extends Dulce{
-    public Super8(int serie) {
-        super(serie);
+/**
+ * Clase que representa un Snickers, un tipo específico de dulce.
+ */
+class Snickers extends Dulce {
+    public Snickers() {
+        // Constructor vacío
     }
 
-    public String comer() {
+    /**
+     * Devuelve el nombre del producto.
+     * 
+     * @return El nombre del dulce, "snickers".
+     */
+    @Override
+    public String getNombre() {
+        return "snickers"; 
+    }
+
+    /**
+     * Devuelve el precio del Snickers.
+     * 
+     * @return El precio del Snickers según el enum PrecioProducto.
+     */
+    @Override
+    public int getPrecio() {
+        return PrecioProducto.SNICKERS.getPrecio();
+    }
+}
+
+/**
+ * Clase que representa un Super8, un tipo específico de dulce.
+ */
+class Super8 extends Dulce {
+    public Super8() {
+        // Constructor vacío
+    }
+
+    /**
+     * Devuelve el nombre del producto.
+     * 
+     * @return El nombre del dulce, "super8".
+     */
+    @Override
+    public String getNombre() {
         return "super8";
+    }
+
+    /**
+     * Devuelve el precio del Super8.
+     * 
+     * @return El precio del Super8 según el enum PrecioProducto.
+     */
+    @Override
+    public int getPrecio() {
+        return PrecioProducto.SUPER8.getPrecio();
     }
 }
